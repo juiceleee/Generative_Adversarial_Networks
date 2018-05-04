acf06ae2bd91fadffb09299ea755cf3793386e96
Ugness <33034982+Ugness@users.noreply.github.com>
2018-05-04 16:54:53 +0900

    changed Net architecture. same as ../DCGAN, add real data in G_loss to train Q_net

--- InfoGAN/torch_model.py
+++ InfoGAN/torch_model.py
@@ -32,121 +32,67 @@ def one_hot(labels):
 
 # for Discriminator
 
-class D_pre(nn.Module):
+class Discriminator_pre(nn.Module):
 
     def __init__(self):
-        super(D_pre, self).__init__()
+        super(Discriminator_pre, self).__init__()
         self.pad = nn.ReplicationPad2d(1)
-        self.lRelu = nn.LeakyReLU(0.1)
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=(4, 4), stride=2)
-        self.conv2 = (nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(4, 4), stride=2))
-        self.bn_conv = (nn.BatchNorm2d(128))
-        self.fc1 = (nn.Linear(in_features=7 * 7 * 128, out_features=1024))
-        self.bn_fc = (nn.BatchNorm1d(1024))
-        for params in self.parameters():
-            if len(params.size()) >= 2:
-                nn.init.xavier_normal_(params.data)
-        # 2 for continuous latent codes, 10 for category.
-        # For this task, we use 1 ten-dimensional categorical code, 2 continuous
-        # latent codes and 62 noise variables, resulting in a concatenated dimension of 74.
+        self.h1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=(3, 3))
+        # self.h1.weight.data.normal_(0, 0.01)
+        self.h2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3))
+        # self.h2.weight.data.normal_(0, 0.01)
+        self.h3 = nn.Linear(in_features=28 * 28 * 64, out_features=625)
+        # self.h3.weight.data.normal_(0, 0.01)
+        # self.h4 = nn.Linear(in_features=625, out_features=1)
+        # self.h4.weight.data.normal_(0, 0.01)
+        # self.dropout = nn.Dropout(p=0.5)
 
     def forward(self, *input):
         x = input[0]
         # x = torch.reshape(x, (-1, 1, 28, 28))
         x = self.pad(x)
-        x = self.conv1(x)
-        x = self.lRelu(x)
+        x = self.h1.forward(x)
+        x = F.relu(x)
         x = self.pad(x)
-        x = self.conv2(x)
-        x = self.lRelu(x)
-        x = self.bn_conv(x)
-        x = torch.reshape(x, (-1, 7 * 7 * 128))
-        x = self.fc1(x)
-        x = self.lRelu(x)
-        x = self.bn_fc(x)
+        x = self.h2.forward(x)
+        x = F.relu(x)
+        x = torch.reshape(x, (-1, 28 * 28 * 64))
+        x = self.h3.forward(x)
+        x = F.relu(x)
+        # x = self.h4.forward(x)
+        # x = self.dropout.forward(x)
+        # x = F.sigmoid(x)
+
         return x
-
-class D_out(nn.Module):
-    def __init__(self):
-        super(D_out, self).__init__()
-        self.fc1 = nn.Linear(1024, 1)
-
-    def forward(self, *input):
-        x = input[0]
-        x = self.fc1(x)
-        x = F.sigmoid(x)
-        return x
-
-class Q_out(nn.Module):
-    def __init__(self):
-        super(Q_out, self).__init__()
-        self.fc1 = nn.Linear(1024, 128)
-        self.bn1 = nn.BatchNorm1d(128)
-        self.lRelu = nn.LeakyReLU(0.1)
-        self.fc_disc = nn.Linear(128, 10)
-        self.fc_cont = nn.Linear(128, 2)
-
-    def forward(self, *input):
-        x = input[0]
-        x = self.fc1(x)
-        x = self.bn1(x)
-        x = self.lRelu(x)
-        disc_out = self.fc_disc(x)
-        cont_out = self.fc_cont(x)
-        return F.softmax(disc_out, dim=1), F.sigmoid(cont_out)
 
 
 class Discriminator(nn.Module):
-
     def __init__(self):
         super(Discriminator, self).__init__()
-        fe1 = list()
-        pad = nn.ReplicationPad2d(1)
-        lRelu = nn.LeakyReLU(0.1)
-        fe1.append(pad)
-        fe1.append(nn.Conv2d(in_channels=1, out_channels=64, kernel_size=(4, 4), stride=2))
-        fe1.append(nn.LeakyReLU(0.1, inplace=True))
-        fe1.append(pad)
-        fe1.append(nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(4, 4), stride=2))
-        fe1.append(nn.LeakyReLU(0.1, inplace=True))
-        fe1.append(nn.BatchNorm2d(128))
-        fe2 = list()
-        fe2.append(nn.Linear(in_features=7 * 7 * 128, out_features=1024))
-        fe2.append(nn.LeakyReLU(0.1, inplace=True))
-        fe2.append(nn.BatchNorm1d(1024))
-        self.FE1 = nn.Sequential(*fe1)
-        self.FE2 = nn.Sequential(*fe2)
-        self.D = nn.Linear(in_features=1024, out_features=1)
-
-        q = list()
-        q.append(nn.Linear(1024, 128))
-        q.append(nn.BatchNorm1d(128))
-        q.append(nn.LeakyReLU(0.1, inplace=True))
-        # q.append(nn.Linear(128, 12))
-        self.Q = nn.Sequential(*q)
-        self.Q_cont = nn.Sequential(nn.Linear(128, 2), nn.Sigmoid())
-        self.Q_disc = nn.Sequential(nn.Linear(128, 10), nn.Softmax(dim=1))
-        for params in self.parameters():
-            if len(params.size()) >= 2:
-                nn.init.xavier_normal_(params.data)
-        # 2 for continuous latent codes, 10 for category.
-        # For this task, we use 1 ten-dimensional categorical code, 2 continuous
-        # latent codes and 62 noise variables, resulting in a concatenated dimension of 74.
+        self.h4 = nn.Linear(in_features=625, out_features=1)
+        self.dropout = nn.Dropout(p=0.5)
 
     def forward(self, *input):
         x = input[0]
-        # x = torch.reshape(x, (-1, 1, 28, 28))
-        x = self.FE1(x)
-        x = torch.reshape(x, (-1, 7 * 7 * 128))
-        x = self.FE2(x)
-        # x = F.dropout(x, 0.3)
-        D = self.D(x)
-        Q = self.Q(x)
-        code_cont = self.Q_cont(Q)
-        code_disc = self.Q_disc(Q)
-        D = F.sigmoid(D)
+        x = self.h4.forward(x)
+        x = self.dropout.forward(x)
+        x = F.sigmoid(x)
 
-        return D, code_disc, code_cont
+        return x
+
+
+class Q_net(nn.Module):
+    def __init__(self):
+        super(Q_net, self).__init__()
+        self.h4 = nn.Linear(in_features=625, out_features=10)
+        self.h5 = nn.Linear(in_features=625, out_features=2)
+
+    def forward(self, *input):
+        x = input[0]
+        disc = self.h4(x)
+        cont = self.h5(x)
+
+        return F.softmax(disc, 1), F.sigmoid(cont)
 
 
 # for Generator
@@ -154,20 +100,13 @@ class Generator(nn.Module):
 
     def __init__(self):
         super(Generator, self).__init__()
-        # self.lRelu = nn.LeakyReLU(0.2)
-        self.fc1 = nn.Linear(noise_n + 12, 1024, True)
-        # nn.init.xavier_normal_(self.fc1.weight)
-        self.bn1 = nn.BatchNorm1d(1024)
-        self.fc2 = nn.Linear(1024, 7 * 7 * 16)
-        self.bn2 = nn.BatchNorm1d(7 * 7 * 16)
-        self.h1 = nn.ConvTranspose2d(16, 4, kernel_size=(2, 2), stride=2)  # 7,7 -> 14,14
-        self.bn3 = nn.BatchNorm2d(4)
-        # nn.init.xavier_normal_(self.h1.weight)
-        self.h2 = nn.ConvTranspose2d(4, 1, kernel_size=(2, 2), stride=2)  # 14,14 -> 28,28
-        # nn.init.xavier_normal_(self.h2.weight)
-        for params in self.parameters():
-            if len(params.size()) >= 2:
-                nn.init.xavier_normal_(params.data)
+        self.projection = nn.Linear(noise_n + 12, 7 * 7 * 16, True)
+        nn.init.xavier_normal_(self.projection.weight)
+        self.h1 = nn.ConvTranspose2d(16, 4, kernel_size=2, stride=2)  # 7,7 -> 14,14
+        nn.init.xavier_normal_(self.h1.weight)
+        self.bn1 = nn.BatchNorm2d(4)
+        self.h2 = nn.ConvTranspose2d(4, 1, kernel_size=2, stride=2)  # 14,14 -> 28,28
+        nn.init.xavier_normal_(self.h2.weight)
         # self.bn2 = nn.BatchNorm1d(512)
         # self.h2.weight.data.normal_(0, 0.01)
         # self.h3 = nn.ConvTranspose2d(16, 1, kernel_size=2, stride=2)
@@ -175,17 +114,15 @@ class Generator(nn.Module):
 
     def forward(self, *input):
         x = torch.cat(input, 1)
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.bn1(x)
-        x = self.fc2(x)
-        x = F.relu(x)
-        x = self.bn2(x)
+        x = self.projection(x)
         x = torch.reshape(x, (-1, 16, 7, 7))
         x = self.h1(x)
         x = F.relu(x)
-        x = self.bn3(x)
+        x = self.bn1(x)
         x = self.h2(x)
+        # x = F.relu(x)
+        # x = self.bn2(x)
+        # x = self.h3(x)
         x = F.sigmoid(x)
         return x
 
@@ -200,16 +137,16 @@ if __name__ == "__main__":
 
     mnist = input_data.read_data_sets("../MNIST_data/")
 
-    Lambda = 0
+    Lambda = 1
     learning_rate = 0.0002
     training_epochs = 1000
     batch_size = 256
     noise_n = 62
     flag = 0
     G = Generator().cuda()
-    D_front = D_pre().cuda()
-    D = D_out().cuda()
-    Q = Q_out().cuda()
+    D_front = Discriminator_pre().cuda()
+    D = Discriminator().cuda()
+    Q = Q_net().cuda()
     opt_G = torch.optim.Adam(
         [{'params': G.parameters()}, {'params': Q.parameters()}],
         lr=0.001)
@@ -255,8 +192,10 @@ if __name__ == "__main__":
             # D_fake, c_disc, c_cont = D(G(noise, label, code))
             D_fake = D(pre_fake)
             c_disc, c_cont = Q(pre_fake)
-            G_loss = -torch.mean(torch.log(D_fake)) - Lambda * (
-                    torch.mean(label * torch.log(c_disc)) + NLL_Gaussian(c_cont))
+            real_c_disc, real_c_cont = Q(D_front(X))
+            class_loss = torch.mean(label * torch.log(c_disc)) + torch.mean(label * torch.log(real_c_disc))
+            cont_loss = NLL_Gaussian(c_cont) + NLL_Gaussian(real_c_cont)
+            G_loss = -torch.mean(torch.log(D_fake)) - Lambda * (class_loss + cont_loss)
             G_loss.backward(retain_graph=True)
             opt_G.step()
             # for name, param in G.named_parameters():
@@ -264,6 +203,8 @@ if __name__ == "__main__":
             if i % 10 == 0:
                 writer.add_scalar("D_loss", D_loss, i + epoch * total_batch)
                 writer.add_scalar("G_loss", G_loss, i + epoch * total_batch)
+                writer.add_scalar("class_loss", class_loss, i + epoch * total_batch)
+                writer.add_scalar("cont_loss", cont_loss, i + epoch * total_batch)
             if i % 100 == 0:
                 print("EPOCH : {}, BATCH: {}\n".format(epoch, i), "D_loss : {}, G_loss : {}".format(D_loss, G_loss))
         test_noise = torch.unsqueeze(noise[batch_size // 2], 0)
